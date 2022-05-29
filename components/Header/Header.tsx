a72bd8d220503ba684ab0c114f4944b822420270
Kim Hưng <leonkyagami@gmail.com>
2022-05-29 12:39:51 +0700

    feat(responsive-header): add menu bar for header for smaller screen

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,21 @@
-import { Box, Button, Flex, Link as CLink } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+    Flex,
+    Link as CLink,
+    useDisclosure,
+    VStack
+} from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
+import { IoMenu } from "react-icons/io5";
 import { Logo } from "../Logo";
 
 export const NavBar = styled(Box)`
@@ -11,10 +24,27 @@ export const NavBar = styled(Box)`
 `;
 
 export const Header: React.FunctionComponent = () => {
-  const router = useRouter();
-  const onConnectWallet = () => {
-    router.push("/me");
-  };
+  const btnRef = React.useRef<HTMLButtonElement>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+    const navItems = (
+    <>
+      <Link href="/auctions" passHref>
+        <CLink px={6} py={2} fontWeight="medium">
+          Auctions
+        </CLink>
+      </Link>
+      <Link href="/exhibitions" passHref>
+        <CLink px={6} py={2} fontWeight="medium">
+          Exhibitions
+        </CLink>
+      </Link>
+        <Link href="/me" passHref>
+          <Button as="a" colorScheme="purple">
+            Connect Wallet
+          </Button>
+        </Link>
+    </>
+    );
   return (
     <NavBar w="full" position="fixed" top="0" left="0" zIndex="100">
       <Flex maxW="container.xl" py={2} px={4} mx="auto">
@@ -23,21 +53,30 @@ export const Header: React.FunctionComponent = () => {
             <Logo />
           </CLink>
         </Link>
-        <Flex ml="auto">
-          <Link href="/auctions" passHref>
-            <CLink px={6} py={2} fontWeight="medium">
-              Auctions
-            </CLink>
-          </Link>
-          <Link href="/exhibitions" passHref>
-            <CLink px={6} py={2} fontWeight="medium">
-              Exhibitions
-            </CLink>
-          </Link>
-          <Button colorScheme="purple" onClick={onConnectWallet}>
-            Connect Wallet
-          </Button>
+        <Flex ml="auto" display={{base: "flex", sm: "none", md: "flex"}}>
+          {navItems}
         </Flex>
+          <Flex ml="auto" display={{base: "none", sm: "flex", md: "none"}}>
+              <Button bgColor="gray.200" ml={2} ref={btnRef} onClick={onOpen}>
+                  <IoMenu size={24} />
+              </Button>
+              <Drawer
+                  isOpen={isOpen}
+                  placement="right"
+                  onClose={onClose}
+                  finalFocusRef={btnRef}
+              >
+                  <DrawerOverlay />
+                  <DrawerContent>
+                      <DrawerCloseButton />
+                      <DrawerHeader>Create your account</DrawerHeader>
+                      <DrawerBody onClick={onClose}>
+                          <VStack>{navItems}</VStack>
+                      </DrawerBody>
+                  </DrawerContent>
+              </Drawer>
+          </Flex>
+
       </Flex>
     </NavBar>
   );
