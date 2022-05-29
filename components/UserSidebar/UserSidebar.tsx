60a0ca7d2821c8c47b10e5bf91fa0c23c869b2e0
Kim Hưng <leonkyagami@gmail.com>
2022-05-29 11:33:49 +0700

    feat(user-profile): mock user data by json

--- components/UserSidebar/UserSidebar.tsx
+++ components/UserSidebar/UserSidebar.tsx
@@ -1,43 +1,34 @@
-import {
-  Avatar,
-  Box,
-  Flex,
-  Heading,
-  HStack,
-  Icon,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Avatar, Box, Flex, HStack, Icon, Stack, Text } from "@chakra-ui/react";
 import { IoDocumentsOutline, IoFolderOpenOutline } from "react-icons/io5";
-export const UserSidebar: React.FunctionComponent = () => (
-  <>
-    <Flex justifyContent="center" mb={4}>
-      <Box width="280px">
-        <Avatar
-          name="KD"
-          size="full"
-          src="https://cdnb.artstation.com/p/assets/images/images/001/452/685/large/ernest-jan-vincent-munoz-totoro-square.jpg?1446673397"
-        />
-      </Box>
-    </Flex>
-    <Box color="gray.600">
-      <Text as="h2" fontWeight="bold" fontSize="xl">
-        Friedrich Engels
-      </Text>
-      <Text as="span" color="gray.500" lineHeight="1">
-        @friedrich
-      </Text>
+import data from "./data.json";
+export const UserSidebar: React.FunctionComponent = () => {
+  const { avatar, address, userName, fullName, createdAt } = data;
+  return (
+    <>
+      <Flex justifyContent="center" mb={4}>
+        <Box width="280px">
+          <Avatar name="KD" size="full" src={avatar} />
+        </Box>
+      </Flex>
+      <Box color="gray.600">
+        <Text as="h2" fontWeight="bold" fontSize="xl">
+          {fullName}
+        </Text>
+        <Text as="span" color="gray.500" lineHeight="1">
+          {`@${userName}`}
+        </Text>
 
-      <Stack spacing="1" mt="2">
-        <HStack fontSize="sm">
-          <Icon as={IoDocumentsOutline} color="gray.500" />
-          <Text>Ontario, Canada</Text>
-        </HStack>
-        <HStack fontSize="sm">
-          <Icon as={IoFolderOpenOutline} color="gray.500" />
-          <Text>July, 2021</Text>
-        </HStack>
-      </Stack>
-    </Box>
-  </>
-);
+        <Stack spacing="1" mt="2">
+          <HStack fontSize="sm">
+            <Icon as={IoDocumentsOutline} color="gray.500" />
+            <Text>{address}</Text>
+          </HStack>
+          <HStack fontSize="sm">
+            <Icon as={IoFolderOpenOutline} color="gray.500" />
+            <Text>{createdAt}</Text>
+          </HStack>
+        </Stack>
+      </Box>
+    </>
+  );
+};
